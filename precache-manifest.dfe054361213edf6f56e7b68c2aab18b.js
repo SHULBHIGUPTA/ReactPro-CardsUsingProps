33333547b66035466530a1b6a2e90475e97b4e3b
SHULBHIGUPTA <shulbhi18@gmail.com>
2022-02-05 20:50:06 -0800

    Updates

---
+++ precache-manifest.dfe054361213edf6f56e7b68c2aab18b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "615082d7aada4c04009b",
+    "url": "/ReactPro-CardsUsingProps/static/css/main.f37727ea.chunk.css"
+  },
+  {
+    "revision": "615082d7aada4c04009b",
+    "url": "/ReactPro-CardsUsingProps/static/js/main.f7faa6be.chunk.js"
+  },
+  {
+    "revision": "8a422aaad790b77fc117",
+    "url": "/ReactPro-CardsUsingProps/static/js/runtime~main.e0629f0d.js"
+  },
+  {
+    "revision": "0e4007777a51aae15166",
+    "url": "/ReactPro-CardsUsingProps/static/js/2.cf2742ac.chunk.js"
+  },
+  {
+    "revision": "4a834e0a576a3341d8c765b42c0c02f6",
+    "url": "/ReactPro-CardsUsingProps/index.html"
+  }
+];
